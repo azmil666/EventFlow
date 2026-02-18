4b8bfce8c2a3f4e7b4f26e55673f85bcdf3c983e
K AKHIL KUMAR <140427573+Akhilkumar24@users.noreply.github.com>
2026-02-18 22:36:56 +0530

    Implement theme toggle and load from localStorage
    Add theme toggling functionality with localStorage support. persistance theme preference using local storage

--- context/Themecontext.jsx
+++ context/Themecontext.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { createContext, useContext, useState } from "react";
+import { useEffect } from "react";
 
 const ThemeContext = createContext();
 
@@ -18,3 +19,24 @@ export function ThemeProvider({ children }) {
 }
 
 export const useTheme = () => useContext(ThemeContext);
+
+useEffect(() => {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    setDarkMode(true);
+    document.documentElement.classList.add("dark");
+  }
+}, []);
+
+const toggleTheme = () => {
+  const newMode = !darkMode;
+  setDarkMode(newMode);
+
+  if (newMode) {
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+  }
+};
